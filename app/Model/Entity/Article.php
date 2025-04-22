0cdf74130d74093d01383d9686eff1de50fae6cc
KonicekDavid <konicekdavid323@gmail.com>
2025-04-22 21:19:03 +0200

    Doctrine initialization

---
+++ app/Model/Entity/Article.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+/**
+ * @author David Koníček
+ */
+
+namespace App\Model\Entity;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'article')]
+class Article {
+
+    /**
+     * @var int
+     */
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int $id;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string')]
+    private string $title;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'text')]
+    private string $content;
+
+    /**
+     * @var User
+     */
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(name: 'author_id', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
+    private User $author;
+
+    /**
+     * @var DateTimeImmutable
+     */
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $createdAt;
+
+    /**
+     * @var DateTimeImmutable
+     */
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $updatedAt;
+}
---
+++ app/Model/Entity/User.php
@@ -0,0 +1,95 @@
+<?php declare(strict_types=1);
+/**
+ * @author David Koníček
+ */
+
+namespace App\Model\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'user')]
+class User {
+
+    /**
+     * @var int
+     */
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int $id;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string', unique: true)]
+    private string $email;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string')]
+    private string $passwordHash;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string')]
+    private string $role;
+
+    /**
+     * @var Collection
+     */
+    #[ORM\OneToMany(mappedBy: 'author', targetEntity: Article::class)]
+    private Collection $articles;
+
+    /**
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPasswordHash(): string {
+        return $this->passwordHash;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string {
+        return $this->role;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getArticles(): Collection {
+        return $this->articles;
+    }
+
+}
